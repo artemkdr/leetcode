d712b6df9558ab2f1f5ed38050b595ae4c7201be
fishbounce <artem.kdr@gmail.com>
2024-03-13 13:38:21 +0100

    Time: 157 ms (44.79%) | Memory: 57.6 MB (63.32%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.cs
+++ 498-diagonal-traverse/diagonal-traverse.cs
@@ -1,10 +1,10 @@
 public class Solution {
-    public int[] FindDiagonalOrder(int[][] mat) {
-        var rslt = new List<int>();
+    public int[] FindDiagonalOrder(int[][] mat) {        
         int m = mat.Length, n = mat[0].Length, x = 0, y = 0, dir = -1;
+        var rslt = new int[n * m];
         int stage = 0; // 0 - jump, 1 - go through
         for (int i = 0; i < m * n; i++) {           
-            rslt.Add(mat[x][y]);        
+            rslt[i] = mat[x][y];        
             if (stage == 0) {
                 stage = m == 1 || n == 1 ? 0 : 1;
                 if (x == 0 || x == m - 1) {
