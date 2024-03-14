d40a54debae23c65940bbd386db9285f28fcc7fe
Artem Kudryavtsev <artem.kdr@gmail.com>
2024-03-14 18:08:56 +0100

    Time: 105 ms (57.60%) | Memory: 46.7 MB (52.07%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cs
+++ 496-next-greater-element-i/next-greater-element-i.cs
@@ -2,35 +2,17 @@ public class Solution {
     public int[] NextGreaterElement(int[] nums1, int[] nums2) {
         int[] ans = new int[nums1.Length];
         var map = new Dictionary<int,int>();
-        var map1 = new Dictionary<int,int>();
-        int prevMin = Int32.MaxValue;;
-        for (int i = 0; i < nums2.Length; i++) {
-            map[nums2[i]] = i;
-            if (nums2[i] > prevMin && prevMin >= 0) {
-                map1[prevMin] = nums2[i];
-                prevMin = nums2[i];
-            } else {
-                prevMin = Math.Min(prevMin, nums2[i]);
+        var stack = new Stack<int>();
+        foreach (int num in nums2) {
+            // it fills the stack until it founds a number greater than the last one in the stack
+            // once it's found it memorizes for all the numbers in the stack this greater number
+            while (stack.Any() && stack.Peek() < num) {
+                map[stack.Pop()] = num;
             }
-            if (i == nums2.Length - 1) {
-                map1[nums2[i]] = -1;
-            }
+            stack.Push(num);
         }
         for (int i = 0; i < nums1.Length; i++) {
-            var el = nums1[i];
-            int gr = -1;
-            if (map1.ContainsKey(el)) {
-               gr = map1[el]; 
-            } else {
-                var k = map[el];
-                for (var j = k + 1; j < nums2.Length; j++) {
-                    if (nums2[j] > el) {
-                        gr = nums2[j];
-                        break;
-                    }
-                }
-            }
-            ans[i] = gr;
+            ans[i] = map.ContainsKey(nums1[i]) ? map[nums1[i]] : -1;
         }
         return ans;
     }
