bbe1e793bac3bc8e0f4b55ca5d2a6fb5e5fb052b
Artem Kudryavtsev <artem.kdr@gmail.com>
2024-03-14 16:20:59 +0100

    Time: 108 ms (45.39%) | Memory: 46.8 MB (43.55%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cs
+++ 496-next-greater-element-i/next-greater-element-i.cs
@@ -1,9 +1,13 @@
 public class Solution {
     public int[] NextGreaterElement(int[] nums1, int[] nums2) {
         int[] ans = new int[nums1.Length];
+        var map = new Dictionary<int,int>();
+        for (int i = 0; i < nums2.Length; i++) {
+            map[nums2[i]] = i;
+        }
         for (int i = 0; i < nums1.Length; i++) {
             var el = nums1[i];
-            var k = Array.IndexOf(nums2, el);
+            var k = map[el];
             var gr = -1;
             for (var j = k + 1; j < nums2.Length; j++) {
                 if (nums2[j] > el) {
