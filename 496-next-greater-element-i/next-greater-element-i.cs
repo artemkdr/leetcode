bdb23987d71ffded5141cdf350d16abba82948d5
Artem Kudryavtsev <artem.kdr@gmail.com>
2024-03-14 16:46:17 +0100

    Time: 116 ms (14.29%) | Memory: 47.2 MB (19.12%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cs
+++ 496-next-greater-element-i/next-greater-element-i.cs
@@ -2,17 +2,29 @@ public class Solution {
     public int[] NextGreaterElement(int[] nums1, int[] nums2) {
         int[] ans = new int[nums1.Length];
         var map = new Dictionary<int,int>();
+        var map1 = new Dictionary<int,int>();
+        int prevMin = -1;
         for (int i = 0; i < nums2.Length; i++) {
             map[nums2[i]] = i;
+            if (nums2[i] > prevMin && prevMin >= 0) {
+                map1[prevMin] = nums2[i];
+                prevMin = nums2[i];
+            } else {
+                prevMin = Math.Min(prevMin, nums2[i]);
+            }
         }
         for (int i = 0; i < nums1.Length; i++) {
             var el = nums1[i];
-            var k = map[el];
-            var gr = -1;
-            for (var j = k + 1; j < nums2.Length; j++) {
-                if (nums2[j] > el) {
-                    gr = nums2[j];
-                    break;
+            int gr = -1;
+            if (map1.ContainsKey(el)) {
+               gr = map1[el]; 
+            } else {
+                var k = map[el];                
+                for (var j = k + 1; j < nums2.Length; j++) {
+                    if (nums2[j] > el) {
+                        gr = nums2[j];
+                        break;
+                    }
                 }
             }
             ans[i] = gr;
