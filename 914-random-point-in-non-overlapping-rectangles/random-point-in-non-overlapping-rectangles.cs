0c5c185a282443dc3de79e87f9a7c6e08cf178e6
Artem Kudryavtsev <artem.kdr@gmail.com>
2024-03-14 15:42:23 +0100

    Time: 195 ms (40.00%) | Memory: 69.3 MB (60.00%) - LeetSync

--- 914-random-point-in-non-overlapping-rectangles/random-point-in-non-overlapping-rectangles.cs
+++ 914-random-point-in-non-overlapping-rectangles/random-point-in-non-overlapping-rectangles.cs
@@ -1,24 +1,20 @@
 public class Solution {
     Random random;
-    //int[][] rects;
-    //List<int[]> points = new List<int[]>();
     int count = 0;
     Dictionary<int,int[]> map = new Dictionary<int,int[]>();
 
     public Solution(int[][] rects) {
         random = new Random();
-        //this.rects = rects;
         foreach (var r in rects) {   
             map[count] = new int[4] { r[0], r[1], Math.Abs(r[2] - r[0]) + 1, Math.Abs(r[3] - r[1]) + 1 };
             count += (Math.Abs(r[2] - r[0]) + 1) * (Math.Abs(r[3] - r[1]) + 1);     
         }
-        Console.WriteLine("count = " + count);
+        //Console.WriteLine("count = " + count);
     }
     
     public int[] Pick() {
         int r = random.Next(count) + 1;
         int prevKey = 0;
-        //Console.WriteLine($"random = {r}");
         foreach (var key in map.Keys) {  
             if (key >= r) {
                 break;
@@ -26,8 +22,6 @@ public class Solution {
             prevKey = key;
         }
         var data = map[prevKey];
-        //var n = random.Next(data[2] * data[3] + 1); //r - prevKey - 1;     
-        //Console.WriteLine($"start point {data[0]}, {data[1]}, n = {n}, w = {data[2]}, h = {data[3]}");        
         return new [] { data[0] + random.Next(data[2]), data[1] + random.Next(data[3]) };
     }
 }
