58d9c607ffb435e5c4dc851a11d6a09b5b600f6d
Artem Kudryavtsev <artem.kdr@gmail.com>
2024-03-20 17:45:57 +0100

    Time: 247 ms (64.99%) | Memory: 61.6 MB (89.79%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cs
+++ 11-container-with-most-water/container-with-most-water.cs
@@ -1,16 +1,30 @@
 public class Solution {
     public int MaxArea(int[] height) {
         var max = 0;
-        var n = height.Length;    
-        var minh = Int32.MaxValue;   
-        for (var i = 0; i < n; i++) {
+        var n = height.Length;
+        int i = 0, j = n - 1;
+        while (i < n && j > i) {
             var lh = height[i];
-            minh = Math.Min(lh, minh);
+            var rh = height[j];
+            var h = Math.Min(lh, rh);
+            var w = j - i;
+            var cmax = w * h;
+            if (cmax > max) {
+                max = cmax;
+            } else if (lh <= rh) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+
+        /*for (var i = 0; i < n; i++) {
+            var lh = height[i];
             for (var j = n - 1; j > i; j--) {
                 var h = Math.Min(lh, height[j]);
                 var w = j - i;
                 var cmax = w * h;
-                // if this current max is less than found max the left height is already a minimum one,
+                // if this current max is less than found max then left height is already a minimum one,
                 // then we can stop here, because it will not be greater on decreasing width
                 if (cmax > max) {
                     max = cmax;
@@ -18,7 +32,7 @@ public class Solution {
                     break;
                 }
             }
-        }
+        }*/
         return max;
     }
 }
